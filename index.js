864a3adb73c055b3ee1fda994ef664fa12bbaa50
Jordan Halfman <jordanhalfman@gmail.com>
2022-06-08 19:06:23 -0700

    commit tree 65b4f5faa6b38c557c41b822a0d89448a8273e08

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class Polygon {
+    constructor(intArray) {
+        this.intArray = intArray;
+    }
+
+    get countSides() {
+       return this.intArray.length;
+    }
+
+    get perimeter() {
+        return this.intArray.reduce((acc, current) => {
+            return acc + current;
+        }, 0)
+        
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        return (this.intArray[0] + this.intArray[1] > this.intArray[2] && this.intArray[0] + this.intArray[2] > this.intArray[1] && this.intArray[2] + this.intArray[1] > this.intArray[0])
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return (this.intArray [0] === this.intArray[1] && this.intArray [0] === this.intArray[2] && this.intArray [0] === this.intArray[3])
+    }
+
+    get area() {
+        return this.intArray[0] * this.intArray[0];
+    }
+}
